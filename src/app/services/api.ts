d1215b1d694cead8c564fc6d39c371a2334a2e49
cachopaweb <cachopaweb@gmail.com>
2024-06-10 16:58:40 -0400

    add baseUrl

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const url = process.env.BASEURL ?? 'http://localhost:3000'
 
-const api = axios.create({ baseURL:  url+'/api' })
+const api = axios.create({ baseURL:  'https://financeiro-financeiro-next.uo41yf.easypanel.host/api' })
 
 
 export {api, url};
