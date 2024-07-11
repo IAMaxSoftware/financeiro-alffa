d600a140db137024b44f0de3bbce0c028f1e89ab
Iago Sauer <iago_ortiz@hotmail.com>
2024-07-11 11:17:22 -0400

    melhorando a API

--- src/app/api/asaas/asaas.service.ts
+++ src/app/api/asaas/asaas.service.ts
@@ -2,10 +2,11 @@ import { LancamentoDto, LancamentoQuery } from "@/app/api/dtos/lancamentos.dto";
 import { MovimentacoesService } from "../movimentacoes/movimentacoes.service";
 import { getIdByEmail } from "../lib/getIdByEmail";
 import prisma from "@/services/database";
+import { AsaasDto } from "../dtos/asaas.dto";
 
 export class AsaasService {
 
-    async create(lancamento: any) {
+    async create(lancamento: AsaasDto) {
 
         try {
             console.log(lancamento);
---
+++ src/app/api/dtos/asaas.dto.ts
@@ -0,0 +1,4 @@
+export interface AsaasDto {
+    value: number;
+    description: string;
+}
