f1399c6a99270d5478661a5c2ce777b8fe62fc8e
Alessandro Dutra <cachopaweb@gmail.com>
2024-07-10 23:40:10 -0400

    add auth

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -33,9 +33,7 @@ export const {
       clientSecret: GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
+          scope: "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar openid"
         },
       },
     }),
