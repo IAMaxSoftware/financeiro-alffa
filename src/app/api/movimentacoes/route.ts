27c0191a4e280818f2cb900cf79ee5bcf8337d44
iagosauer <iago_ortiz@hotmail.com>
2024-07-11 20:10:53 -0400

    filtrando movimentações por empresa

--- src/app/api/movimentacoes/route.ts
+++ src/app/api/movimentacoes/route.ts
@@ -6,7 +6,7 @@ async function GET(request: NextRequest) {
         const searchParams = request.nextUrl.searchParams
         const dataInicial = (searchParams.get('dataInicial') && new Date(searchParams.get('dataInicial') || '')) || undefined
         const dataFinal = (searchParams.get('dataFinal') && new Date(searchParams.get('dataFinal') || '')) || undefined
-        const empresaId = '1'; //searchParams.get('empresaId') || ''
+        const empresaId = searchParams.get('empresaId') || undefined
         const movimentacoesService = new MovimentacoesService();
         const response = await movimentacoesService.getMovimentacoes({
             dataInicial,
